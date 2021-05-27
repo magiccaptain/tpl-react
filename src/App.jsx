644a9c28d6f69d94922ced2bc99290ae3a5ff4da
magiccaptain <magic--510@hotmail.com>
2021-05-27 10:40:29 +0800

    chore: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "src/App.less";
-import { Button } from "antd";
 import { Layout } from "src/components/Layouts";
 
 function App() {
   return (
     <Layout>
-      <Button type="primary">测试</Button>
+      <p>Hello antd</p>
     </Layout>
   );
 }
